6344644145baaf64098a2713db38ec46b23159f0
Oleg Digital <boss@agerasiadigital.ru>
2025-09-14 17:57:51 +0300

    feat: Контейнеризация

--- src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.catsgram;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+// com.google.gson.Gson;
+//import com.google.gson.JsonSyntaxException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-import java.util.Scanner;
+//import java.util.Map;
+//import java.util.Scanner;
 
 @SpringBootApplication
 public class CatsgramApplication {
